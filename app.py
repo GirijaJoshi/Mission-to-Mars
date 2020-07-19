fc9802ed92a5fd3f12c21904c658da33ad782aaa
Girija Joshi <girija_aj@yahoo.com>
2020-07-18 21:51:42 -0700

    changed app.py to remove extra stuff

--- app.py
+++ app.py
@@ -24,11 +24,6 @@ def scrape():
    # return render_template("index.html", mars=mars)
    return render_template("return.html", mars=mars)
 
-@app.route("/hemisphere")
-def hemisphere():
-    scraping.get_mars_hemisphere()
-    return "Hemisphere Scraping Successful!"
-
 if __name__ == "__main__":
    app.run()
 
